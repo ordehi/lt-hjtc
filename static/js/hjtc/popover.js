67768afc0dc457ec6f1ae8c8a2a6c7397172c8eb
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-03 21:26:52 -0400

    a bunch of cool stuff

--- static/js/hjtc/popover.js
+++ static/js/hjtc/popover.js
@@ -1,32 +1,64 @@
-import pieces from '/store/pieces.json' assert { type: 'json' };
+import content from '/store/content.json' assert { type: 'json' };
 
-const popoverList = Object.keys(pieces).map((id) => {
+function handlePopoverShown(event) {
+  let id = '#' + event.target.getAttribute('aria-describedby');
+  let popover = document.querySelector(id);
+  if (popover) {
+    let tooltipEls = popover.querySelectorAll('.line');
+    if (tooltipEls) {
+      let tooltipList = [...tooltipEls].map((triggerEl) => {
+        let tipId = '#' + triggerEl.id;
+        const tooltip = bootstrap.Tooltip.getOrCreateInstance(tipId);
+        const options = {
+          placement: 'top',
+          trigger: 'hover focus',
+          html: true,
+          title: content['tc-lines'][triggerEl.id],
+          customClass: 'custom-tooltip',
+          delay: { show: 0, hide: 500 },
+        };
+
+        tooltip._config = { ...tooltip._config, ...options };
+        return tooltip;
+      });
+    }
+  }
+}
+
+const popoverTriggerList = document.querySelectorAll('.piece');
+const popoverList = [...popoverTriggerList].map((triggerEl) => {
+  let id = triggerEl.id;
   const popover = bootstrap.Popover.getOrCreateInstance('#' + id);
   const options = {
     placement: 'top',
     trigger: 'click',
     html: true,
-    title: pieces[id].title,
-    content: pieces[id].content,
+    title: content.pieces[id].title,
+    content: content.pieces[id].content,
     customClass: 'custom-popover',
   };
 
   popover._config = { ...popover._config, ...options };
+  triggerEl.addEventListener('shown.bs.popover', (event) =>
+    handlePopoverShown(event)
+  );
   return popover;
 });
-// This is where the magic starts happening! At this point, we have a complete and working statement that has some of the barebones functionality that ships with Hotjar.
 
-// Try it out yourself, run this line of code in your browser Console and then try sending anything using the `hj()` function, it can be an Event as you might have seen such as `hj('event', 'my_wondeful_event')` or a flerbo `hj('flerbo', 'bleepbloop')` or even an emoji `hj('🥓', 'tasty')` Unfortunately, sending bacon to Hotjar, or anything not in our APIs, won't do anything once Hotjar is loaded, the command would just be rejected, but it's a cool way to start understanding how Hotjar works under the hood.
+const tpRegex = /(tooltip|popover)/;
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', (event) => {
     let openList = document.querySelectorAll('.popover');
-    if (openList) {
-      openList.forEach((popover) => {
-        let id =
-          '#' + document.querySelector(`[aria-describedby=${popover.id}]`).id;
-        bootstrap.Popover.getInstance(id).hide();
-      });
+    if (openList.length) {
+      let classList = event.target.parentElement?.classList;
+      if (!classList || (classList && !tpRegex.test(classList[0]))) {
+        openList.forEach((popover) => {
+          let id =
+            '#' + document.querySelector(`[aria-describedby=${popover.id}]`).id;
+          bootstrap.Popover.getInstance(id).hide();
+        });
+      }
     }
   });
 });
